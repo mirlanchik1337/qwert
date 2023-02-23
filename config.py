3a35f88d9624b2f62c94ccbdf2cf4fb46618234f
mirlanchik1337 <122021627+mirlanchik1337@users.noreply.github.com>
2023-02-23 15:10:03 +0600

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Dispatcher, Bot
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-bot = Bot(token=os.getenv('BOT_TOKEN'))
-db = Dispatcher(bot)
-ADMIN = [5416111170, 5416111170]
